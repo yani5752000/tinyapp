24318ceb43b50a9c539ba2ef6dd311d0ad12471c
Farid Asadpour <yani5752000@yahoo.com>
2021-07-06 02:26:31 +0000

    express_server.js redirection

--- express_server.js
+++ express_server.js
@@ -43,9 +43,18 @@ app.get("/urls/:shortURL", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
+});
+
 app.post("/urls", (req, res) => {
   console.log(req.body);  // Log the POST request body to the console
-  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+  const shortURL = generateRandomString();
+  urlDatabase[shortURL] = req.body.longURL;
+  res.redirect("/urls/:" + shortURL);
+  console.log(urlDatabase);
+  //res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
 
 app.listen(PORT, () => {
