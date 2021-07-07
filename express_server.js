119f08cffdc18ec6bdb413ebfe7be8c8ca930da3
Farid Asadpour <yani5752000@yahoo.com>
2021-07-07 01:29:44 +0000

    feature/cookie

--- express_server.js
+++ express_server.js
@@ -6,7 +6,9 @@ function generateRandomString() {
 }
 
 const express = require("express");
+var cookieParser = require('cookie-parser')
 const app = express();
+app.use(cookieParser())
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
 const PORT = 8080; // default port 8080
@@ -30,16 +32,21 @@ app.get("/hello", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = { urls: urlDatabase, username: req.cookies["username"], };
+  
+  
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = {username: req.cookies["username"] };
+  
+  res.render("urls_new", templateVars);
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL], username: req.cookies["username"] };
+  
   res.render("urls_show", templateVars);
 });
 
@@ -65,6 +72,26 @@ app.post("/urls", (req, res) => {
   console.log(urlDatabase);
   //res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
+
+app.post("/login", (req, res) => {
+  res.cookie("username", req.body.username);
+  
+  //console.log(req.body);
+  res.redirect("/urls");
+  
+});
+
+app.post("/logout", (req, res) => {
+  
+  
+  res.clearCookie("username");
+  
+  res.redirect("/urls");
+  
+});
+
+
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
